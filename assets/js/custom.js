bda07c5f587c0ee19812cb0162292fd5d8e3ba2e
Kushen De Silva <kushenthimira@gmail.com>
2021-03-08 15:47:00 +0530

    build v2.4
    UI Update

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -5,6 +5,22 @@
 // Developer:    :  Cipher
 // ==================================================
 
+function readMoreFunction() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read More";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read Less";
+    moreText.style.display = "inline";
+  }
+} 
+
 (function ($) {
   "use strict";
 
